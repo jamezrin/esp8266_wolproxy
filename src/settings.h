03d46f6e32d93e8395307d150dad81e9ba3d2d44
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2018-01-29 15:29:54 +0100

    Added updated settings.h

--- src/settings.h
+++ src/settings.h
@@ -1,17 +1,19 @@
 #include <ESP8266WiFi.h>
 
-const char* hostname = "WakeOnLan";
-const char* ssid = "YourWiFiAP";
-const char* password = "superpass123";
-const int port = 5009;
+namespace Settings {
+  static const char* hostname = "WakeOnLan";
+  static const char* ssid = "YourWiFiAP";
+  static const char* password = "superpass123";
+  static const int port = 5009;
 
-const bool staticConnection = true;
-const IPAddress address = IPAddress(192, 168, 0, 10);
-const IPAddress gateway = IPAddress(192, 168, 0, 1);
-const IPAddress netmask = IPAddress(255, 255, 255, 0);
+  static const bool staticConnection = true;
+  static const IPAddress address = IPAddress(192, 168, 0, 10);
+  static const IPAddress gateway = IPAddress(192, 168, 0, 1);
+  static const IPAddress netmask = IPAddress(255, 255, 255, 0);
 
-const IPAddress targetAddress = IPAddress(255, 255, 255, 255);
-const int targetPort = 9;
+  static const IPAddress targetAddress = IPAddress(255, 255, 255, 255);
+  static const int targetPort = 9;
 
-const char secureOn[6] = {0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6};
-const bool enforcePassword = true;
+  static const char secureOn[6] = {0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6};
+  static const bool enforcePassword = true;
+};
